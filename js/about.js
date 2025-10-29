f7ab9b78c2a21d18a427df70aaec22189c661263
Bowen Wang <bowenwang0815@gmail.com>
2025-10-29 00:33:41 -0700

    finished first point

--- js/about.js
+++ js/about.js
@@ -12,6 +12,21 @@ function parseTweets(runkeeper_tweets) {
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
 	document.getElementById('numberTweets').innerText = tweet_array.length;	
+
+	// get all the tweet times
+	const tweetTimes = tweet_array.map(function(t) 
+	{ 
+		return t.time.getTime(); 
+	});
+	const earliestDate = new Date(Math.min.apply(null, tweetTimes));
+	const latestDate = new Date(Math.max.apply(null, tweetTimes));
+	const dateFormatOptions = 
+	{ 	weekday: 'long', 
+		year: 'numeric', 
+		month: 'long', 
+		day: 'numeric' };
+	document.getElementById('firstDate').innerText = earliestDate.toLocaleDateString('en-US', dateFormatOptions);
+	document.getElementById('lastDate').innerText = latestDate.toLocaleDateString('en-US', dateFormatOptions);
 }
 
 //Wait for the DOM to load
